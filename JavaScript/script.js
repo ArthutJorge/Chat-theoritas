fd8fe17d92865ea9f8a14db02168684d2834565c
ArthurGamaJorge <arthurgamajorgetec@gmail.com>
2023-08-26 01:55:11 -0300

    Transformar logo em png & corrigir botões

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,9 +1,3 @@
-let menu = document.querySelector("#IconeMenu");
-let ListaVertical = document.querySelector(".ListaVertical");
 
-menu.onclick = () => {
-  menu.classList.toggle("bx-x");
-  ListaVertical.classList.toggle("open");
-};
 
 // PARA DE ESPIAR NÃO FAÇO ENIGMA TO SÓ FAZENDO UM SITEZINHO 
