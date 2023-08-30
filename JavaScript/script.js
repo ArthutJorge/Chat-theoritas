91b8cef7ef9603e10e2df9c4c788b779a7fc602d
ArthurGamaJorge <arthurgamajorgetec@gmail.com>
2023-08-29 23:06:58 -0300

    Mudar carrossel - avançar 1 item apenas

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,26 @@
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel, {
+   interval: 200000
+ })
+
+ $('.carousel .carousel-item').each(function(){
+    var minPerSlide = 4;
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    
+    for (var i=0;i<minPerSlide;i++) {
+        next=next.next();
+        if (!next.length) {
+        	next = $(this).siblings(':first');
+      	}
+        
+        next.children(':first-child').clone().appendTo($(this));
+      }
+});
+
 
 
 // PARA DE ESPIAR NÃO FAÇO ENIGMA TO SÓ FAZENDO UM SITEZINHO 
