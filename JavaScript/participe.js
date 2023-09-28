22626e5168f81d7c3fbef6b48109d371852e4268
ArthurGamaJorge <arthurgamajorgetec@gmail.com>
2023-09-28 01:06:09 -0300

    Corrigir link formulários

--- JavaScript/participe.js
+++ JavaScript/participe.js
@@ -1,19 +1,23 @@
 var translations = {
     pt: {
           participe: "Participe",
-          formText: "Form - Português"
+          formText: "Form - Português",
+          link: "https://forms.gle/YTyn8VSSECP728qD9"
     },
     en: {
           participe: "Join us",
-          formText: "Form - English"
+          formText: "Form - English",
+          link: "https://forms.gle/yLPedujnU61TCSx98"
         },
     es: {
           participe: "Participar",
-          formText: "Form - Spanish"
+          formText: "Form - Spanish",
+          link: "https://forms.gle/59iVNysjcV2xTA2Y7"
     },
     fr: {
           participe: "Participer",
-          formText: "Form - Francés"
+          formText: "Form - Francés",
+          link: "https://docs.google.com/forms/d/1Rt_VSLblypf659zAtqu8DtzcRD66WCiraWfiPbNcDDA/"
     }
   };
 function updateLanguage(language) {
@@ -26,7 +30,7 @@ function updateLanguage(language) {
     languageNameElement.textContent = translations[language][language];
   }
   if (participeElement) {
-    participeElement.innerHTML = `<button class="botao" onclick="openForm('${translations[language].formUrl}')"  width="150" height="150" >${translations[language].formText}</button>`;
+    participeElement.innerHTML = `<button class="botao" onclick="openForm('${translations[language].link}')"  width="150" height="150" >${translations[language].formText}</button>`;
     }
        const url = `participe_${language}.html`;
   document.getElementById("content").innerHTML = `<iframe src="./Content/${url}" frameborder="0" width=1200 height="800"></iframe>`;
