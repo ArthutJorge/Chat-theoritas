a09342f0cde52c37da6139ef037a7c39c02ddb1f
ArthurGamaJorge <arthurgamajorgetec@gmail.com>
2023-09-30 11:02:05 -0300

    Corrigir script de membros

--- JavaScript/membros.js
+++ JavaScript/membros.js
@@ -8,17 +8,18 @@ body = document.body
 
 function ChangeClick(imagem){
     if(window.matchMedia("(max-width: 500px)".matches)){
-        if(body.classList.toggle("mobile", true) == true){
-            body.classList.toggle("mobile");
-        }
+        body.classList.remove("mobile");
         
-        if(imagemAnterior == imagem && imagemAnterior != "" && contador <= 2){
+        if(imagemAnterior == imagem && imagemAnterior != "" && contador < 2){
             contador += 1
             body.classList.toggle("mobile");
         } else{
+            if(contador == 2){imagem = ""}
             contador = 1
+            body.classList.remove("mobile");
         }
         imagemAnterior = imagem
+        event.stopPropagation();
 
 }
 }
