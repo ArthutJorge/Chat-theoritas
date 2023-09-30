1ea6bc64f63cfefc8c455f107582e7926ca30b6a
ArthurGamaJorge <arthurgamajorgetec@gmail.com>
2023-09-30 02:57:49 -0300

    Corrigir cards membros

--- JavaScript/membros.js
+++ JavaScript/membros.js
@@ -8,18 +8,18 @@ body = document.body
 
 function ChangeClick(imagem){
     if(window.matchMedia("(max-width: 500px)".matches)){
-
-    if(imagemAnterior == imagem && imagemAnterior != "" && contador <= 2){
-        contador += 1
-        body.classList.toggle("mobile");
-    } else{
-        contador = 1
         if(body.classList.toggle("mobile", true) == true){
             body.classList.toggle("mobile");
         }
-    }
-    imagemAnterior = imagem
-    event.stopPropagation();
+        
+        if(imagemAnterior == imagem && imagemAnterior != "" && contador <= 2){
+            contador += 1
+            body.classList.toggle("mobile");
+        } else{
+            contador = 1
+        }
+        imagemAnterior = imagem
+        event.stopPropagation();
 
 }
 }
