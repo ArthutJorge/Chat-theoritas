907d1c5cd18171f23397dc874e21a7cc4d4deed1
ArthurGamaJorge <arthurgamajorgetec@gmail.com>
2023-09-30 14:26:05 -0300

    corrigir erros

--- JavaScript/membros.js
+++ JavaScript/membros.js
@@ -4,7 +4,7 @@ imagemAnterior = "a"
 function ChangeClick(imagem){
     if(window.matchMedia("(max-width: 900px)".matches)){
         card = document.querySelector(`.a${imagem}`)
-        cardAnterior = document.querySelector(`.a${imagem}`)
+        cardAnterior = document.querySelector(`.a${imagemAnterior}`)
 
         cardAnterior.classList.remove("mobile");
         
@@ -12,7 +12,7 @@ function ChangeClick(imagem){
             contador += 1
             card.classList.toggle("mobile");
         } else{
-            if(imagemAnterior == imagem){imagem = ""}
+            if(imagemAnterior == imagem){imagem = "a"}
             contador = 1
             card.classList.remove("mobile");
         }
