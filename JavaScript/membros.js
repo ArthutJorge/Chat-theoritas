ea2382363cc893615ad9de0ff4346842950792f0
ArthurGamaJorge <arthurgamajorgetec@gmail.com>
2023-09-30 11:46:05 -0300

    Corrigir responsividade navbar

--- JavaScript/membros.js
+++ JavaScript/membros.js
@@ -1,21 +1,16 @@
-
 contador = 1;
 imagemAnterior = ""
 body = document.body
-imagemPreAnterior = ""
-
-
 
 function ChangeClick(imagem){
     if(window.matchMedia("(max-width: 500px)".matches)){
         body.classList.remove("mobile");
         
-        if(imagemAnterior == imagem && imagemAnterior != "" && contador < 2 && imagemPreAnterior != imagemAnterior){
+        if(imagemAnterior == imagem && imagemAnterior != "" && contador < 2){
             contador += 1
             body.classList.toggle("mobile");
         } else{
-            imagemPreAnterior = ""
-            if(contador == 2){imagemPreAnterior = imagemAnterior}
+            if(imagemAnterior == imagem){imagem = ""}
             contador = 1
             body.classList.remove("mobile");
         }
